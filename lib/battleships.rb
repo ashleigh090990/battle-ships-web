8807bcc75720ce60ebe816df0ad9b14e16b6e623
Ashleigh <ashleighmaund@gmail.com>
2015-05-12 13:46:28 +0100

    added first test for battleships web - link to start new game

---
+++ lib/battleships.rb
@@ -0,0 +1,12 @@
+require 'sinatra/base'
+
+class Battleships < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  set views: proc{ File.join(root, '..', 'views') }
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end

bbe72391d97bcbe88776479a8ead7a417535f5d8
Ashleigh <ashleighmaund@gmail.com>
2015-05-12 18:31:18 +0100

    added to Registering test - still passing

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -9,6 +9,15 @@ class Battleships < Sinatra::Base
   	erb :game
   end
 
+  get '/no_name_provided' do
+  	@name = 
+    erb :no_name_provided
+  end
+
+  get '/start_game' do
+  	erb :start_game
+  end
+
   set views: proc{ File.join(root, '..', 'views') }
 
   # start the server if ruby file executed directly
--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -5,11 +5,15 @@ feature 'Starting a new game' do
   	visit '/'
   	click_link 'New Game'
   	expect(page).to have_content "What is your name?"
-  	fill_in('First Name', with: 'Alejandro')
+  	fill_in('Name', with: 'Alejandro')
   	find_button('Submit').click
+  	expect(page).to have_content "You made it!"
   end
 
-  scenario 'Player does not provide name' do
-
-  end
+  # scenario 'Assign name if player does not provide name' do
+  #   visit '/game'
+  #   fill_in('Name', with: '')
+  #   find_button('Submit').click
+  #   expect(page).to have_content "You didn't provide a name!"
+  # end
 end
